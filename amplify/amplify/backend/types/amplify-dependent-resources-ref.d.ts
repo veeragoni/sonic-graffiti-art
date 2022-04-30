6e5d2434630652f0f02ca5fbc1f2ccdbd7114f6c
ujwbukka <ujwbukka@amazon.com>
2022-04-30 19:21:40 -0400

    Added Login and using temp credentials to access transcribe

---
+++ amplify/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "sonicgraffitiamplifyf08566d2": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
